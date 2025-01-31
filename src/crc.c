a5dc6f4447fde81aa8475bb46603fe9e4413165d
Yuki Hoshino <83908969+DumberThanUsual@users.noreply.github.com>
2025-01-31 16:36:24 +0000

    Fixed CRC infinite loop

--- src/crc.c
+++ src/crc.c
@@ -11,7 +11,7 @@ uint16_t crc16(uint8_t *ptr, uint16_t count)
 {
     uint16_t crc = 0;
 
-    while(--count >= 0 )
+    while (count-- > 0)
     {
         crc = crc ^ ((uint16_t) (*ptr++ << 8));
         for(uint8_t i = 0; i < 8; i++)
